86007ba64148f1856de2ae01881619b45e72be48
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-06-15 22:01:26 +0900

    WorldAnchorをアタッチするようにしてみた。

--- MRBook/Assets/MRBook/Programmer/Vuforia/HoloTracableEventHandler.cs
+++ MRBook/Assets/MRBook/Programmer/Vuforia/HoloTracableEventHandler.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Vuforia;
+using UnityEngine.VR.WSA;
+using UnityEngine.VR.WSA.Persistence;
 
 public class HoloTracableEventHandler : MyTracableEventHandler
 {
@@ -12,21 +14,44 @@ public class HoloTracableEventHandler : MyTracableEventHandler
     [SerializeField]
     Transform anchorTransform = null;
 
+    [SerializeField]
+    Vector3 offset = new Vector3(0.0f, -0.1f, 0.0f);
+
+    WorldAnchorStore anchorStore;
+
     protected override void OnTrackingFound()
     {
         //一回のみ生成
         if (isView) return;
         isView = true;
         Vector3 pos;
-        //WorldAnchorの位置に生成する
+        //WorldAnchorの位置に移動
         for (int i = 0; i < objArray.Length; i++)
         {
             objArray[i].SetActive(true);
             pos = anchorTransform.position + objArray[i].transform.position;
-            objArray[i].transform.position = pos + (Vector3.up * -0.1f);
+            objArray[i].transform.position = pos + offset;
             objArray[i].transform.rotation = anchorTransform.rotation;
+            WorldAnchor attaching = objArray[i].transform.Find("stage").gameObject.AddComponent<WorldAnchor>();
+            attaching.OnTrackingChanged += AttachingAnchor_OnTrackingChanged;
         }
         VuforiaBehaviour.Instance.enabled = false;
+
+
+    }
+
+    void AnchorStoreReady(WorldAnchorStore anchorStore)
+    {
+        this.anchorStore = anchorStore;
+    }
+
+    void AttachingAnchor_OnTrackingChanged(WorldAnchor self, bool located)
+    {
+        if (!located) return;
+
+        anchorStore.Save(self.gameObject.name, self);
+
+        self.OnTrackingChanged -= AttachingAnchor_OnTrackingChanged;
     }
 
     protected override void OnTrackingLost() { }
