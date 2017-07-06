21ebe357f0882878c6bd30b620202320cebbd477
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-07-06 14:27:02 +0900

    動的ナビメッシュのためのアセットを追加。

---
+++ MRBook/Assets/MRBook/Programmer/UI/HoloText.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(TextMesh))]
+public class HoloText : MonoBehaviour
+{
+    TextMesh mesh;
+    string currentText;
+    public string CurrentText
+    {
+        get { return currentText; }
+        set
+        {
+            if (currentText == value) return;
+            currentText = value;
+            if (mesh != null) mesh.text = currentText;
+        }
+    }
+
+    protected virtual void Start()
+    {
+        mesh = GetComponent<TextMesh>();
+        CurrentText = mesh.text;
+    }
+}
