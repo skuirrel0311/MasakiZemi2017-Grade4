1a41c23fb081603bd64883a06c39e7aabb735235
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-08-02 20:24:25 +0900

    オブジェクトのコントロール時に使うシェーダーを書いた。

---
+++ MRBook/Assets/MRBook/Programmer/Shaders/AnchorInstancingShader.shader
@@ -0,0 +1,33 @@
+﻿Shader "Custom/AnchorInstancingShader" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+	}
+	SubShader {
+		Tags { "RenderType"="Opaque" }
+		
+		CGPROGRAM
+		#pragma surface surf Standard fullforwardshadows
+
+		#pragma target 3.0
+
+		#pragma multi_comple_instancing
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		UNITY_INSTANCING_CBUFFER_START(Props)
+			UNITY_DEFINE_INSTANCED_PROP(fixed4, _Color)
+		UNITY_INSTANCING_CBUFFER_END
+
+		void surf (Input IN, inout SurfaceOutputStandard o) {
+			fixed4 c = UNITY_ACCESS_INSTANCED_PROP(_Color);
+			o.Albedo = c.rgb;
+			o.Alpha = c.a;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
---
+++ MRBook/Assets/MRBook/Programmer/Test/ChangeColor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    List<Renderer> renderers = new List<Renderer>();
+
+    void Start()
+    {
+        MaterialPropertyBlock block = new MaterialPropertyBlock();
+        renderers.AddRange(GetComponentsInChildren<Renderer>());
+
+        Color c = new Color();
+
+        foreach (Renderer r in renderers)
+        {
+            c.r = Random.Range(0.0f, 1.0f);
+            c.g = Random.Range(0.0f, 1.0f);
+            c.b = Random.Range(0.0f, 1.0f);
+            block.SetColor("_Color", c);
+
+            r.SetPropertyBlock(block);
+        }
+    }
+}
