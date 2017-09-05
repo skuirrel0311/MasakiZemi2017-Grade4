7c57eb4ce62433288a8b8230b150d076027dbc16
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-09-05 16:11:16 +0900

    タイトル追加途中

---
+++ MRBook/Assets/MRBook/Programmer/GameManager/TitleSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSceneManager : BaseManager<TitleSceneManager>
+{
+    public void GameStart()
+    {
+        SceneManager.LoadSceneAsync("main");
+    }
+}
