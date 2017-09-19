e314e57580ec65900594b8c8a7be84b12dbf354e
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-09-19 17:43:10 +0900

    作業途中

---
+++ MRBook/Assets/MRBook/Programmer/ObjectController/BookPositionModifier.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR.WSA;
+
+public class BookPositionModifier : BaseManager<BookPositionModifier>
+{
+    MyGameManager gameManager;
+    MainSceneManager mainSceneManager;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        gameManager = MyGameManager.I;
+        mainSceneManager = MainSceneManager.I;
+
+        WorldManager.OnPositionalLocatorStateChanged += WorldManagerOnStateChanged;
+    }
+
+    void WorldManagerOnStateChanged(PositionalLocatorState oldState, PositionalLocatorState newState)
+    {
+        //アクティブに戻った時になおす
+        if (newState == PositionalLocatorState.Active)
+        {
+            ModifyBookPosition(true);
+        }
+    }
+
+    public void ModifyBookPosition(bool showDialog)
+    {
+        if (gameManager.currentSceneState != MyGameManager.SceneState.Main) return;
+        //MainSceneManager.I.SetBookPositionByAnchor(bookTransform.position, bookTransform.rotation);
+        if (showDialog) NotificationManager.I.ShowDialog("警告", "ホログラムのずれを検知しました。", true, 3.0f);
+    }
+}
