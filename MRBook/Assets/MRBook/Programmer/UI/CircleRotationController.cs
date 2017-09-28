4863f87ac3b3a140b61b938b853543a95f539de6
DESKTOP-SAS74A2\HaneishiNanase <himono3331@gmail.com>
2017-09-28 19:31:05 +0900

    【design】サークル素材あげました。

---
+++ MRBook/Assets/MRBook/Programmer/UI/CircleRotationController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleRotationController : MonoBehaviour {
+
+    [SerializeField]
+    float rotVal = 15.0f;
+
+
+    [SerializeField]
+    float interval = 0.25f;
+
+    Transform m_transform;
+    Vector3 rot;
+
+    void Awake()
+    {
+        m_transform = transform;
+        rot = Vector3.zero;
+    }
+
+    void Start()
+    {
+        StartCoroutine(Rotation());
+    }
+
+    IEnumerator Rotation()
+    {
+        while(true)
+        {
+            rot.y += rotVal;
+            if (rot.y >= 360.0f)
+            {
+                rot.y = 0.0f;
+            }
+            m_transform.rotation = Quaternion.Euler(rot);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
