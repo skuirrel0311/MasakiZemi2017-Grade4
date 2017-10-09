11fd1415a6a3531d826122eff2a8de9219fa55f3
skuirrel0311 <rureikus7223@gmail.com>
2017-10-09 13:35:54 +0900

    作業途中

---
+++ MRBook/Assets/MRBook/Editor/MenuTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuTest : EditorWindow
+{
+    int index = 0;
+    Vector2 scroll;
+
+    void OnGUI()
+    {
+        scroll = EditorGUILayout.BeginScrollView(scroll);
+
+        index = GUIExtension.Menu(new List<string> {
+            "水素",
+            "ヘリウム",
+            "リチウム",
+            "ベリリウム",
+            "ホウ素",
+            "炭素",
+            "窒素",
+            "酸素",
+            "フッ素",
+            "ネオン",
+        }, index);
+
+        EditorGUILayout.EndScrollView();
+    }
+
+    [MenuItem("Window/MenuTest")]
+    public static void ShowWindow()
+    {
+        GetWindow<MenuTest>();
+    }
+}
+
+public static class GUIExtension
+{
+
+    public static int Menu(List<string> textList, int index)
+    {
+        for (int i = 0; i < textList.Count; i++)
+        {
+            bool flag = GUILayout.Toggle(index == i, textList[i], "OL Elem");
+ 
+            if (flag != (index == i))
+            {
+                return i;
+            }
+        }
+ 
+        return index;
+    }
+}
---
+++ MRBook/Assets/MRBook/Programmer/StateMachineBehavior/SetEnding.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetEnding : StateMachineBehaviour
+{
+    public string endingName;
+
+
+}
