1b79de9fe7a064f61381a0d041617e47a39fcce1
Kazuki <k.kaneko0311@gmail.com>
2017-11-17 12:54:24 +0900

    別環境で作業するため、一旦コミット

---
+++ MRBook/Assets/MRBook/Programmer/StateMachineBehavior/KillPuppet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPuppet : BaseStateMachineBehaviour
+{
+    [SerializeField]
+    ActorName puppetName = ActorName.Urashima;
+
+    public override void OnStart(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStart(animator, stateInfo, layerIndex);
+
+        HoloPuppet puppet = (HoloPuppet)ActorManager.I.GetCharacter(puppetName);
+
+        puppet.puppet.state = RootMotion.Dynamics.PuppetMaster.State.Dead;
+    }
+}
