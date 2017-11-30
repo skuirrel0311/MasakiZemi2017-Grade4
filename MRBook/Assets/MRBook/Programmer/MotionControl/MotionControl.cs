d2808967f40064083139afc1fd0f2d3e3952a3f9
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-11-30 11:45:59 +0900

    上げ忘れ

--- MRBook/Assets/MRBook/Programmer/MotionControl/MotionControl.cs
+++ MRBook/Assets/MRBook/Programmer/MotionControl/MotionControl.cs
@@ -3,26 +3,6 @@
 [System.Serializable]
 public enum MotionName
 {
-<<<<<<< .merge_file_a11732
-    Wait,               //待機
-    Walk,               //歩く
-    Talk,               //話す
-    Drink,              //飲む
-    Eat,                //食べる
-    Sit,                //座っている
-    StandSit,           //立った状態から座るモーション
-    Stand,              //立つ
-    Swing,              //釣竿を振る
-    Performance,        //演奏する
-    Dance,              //踊る
-    Lie,                //寝っ転がる
-    Maintenance,        //釣竿の手入れ
-    Troubled,           //困惑する
-    PutOff,             //引く
-    Dead,               //死亡
-    OpenDoorFailure,    //ドア開け失敗
-    Peck                //スズメが突くモーション
-=======
     Wait,       //待機
     Walk,       //歩く
     Talk,       //話す
@@ -41,10 +21,7 @@ public enum MotionName
     Dead,       //死亡
     OpenDoorFailure,//ドア開け失敗
     FishingThrow,   //釣り始め
-    FishingLoop,    //釣りループ
     FishingDead     //釣りして引きずり込まれて死亡
-
->>>>>>> .merge_file_a01960
 }
 
 public static class MotionNameManager
