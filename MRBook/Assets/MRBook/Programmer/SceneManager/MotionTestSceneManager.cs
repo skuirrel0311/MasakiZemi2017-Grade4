348ab893e14bbfe442e47d3ef3e2b9200919f78a
Kazuki <k.kaneko0311@gmail.com>
2017-12-06 15:34:58 +0900

    モーション確認用シーンのスクリプトのみを修正

--- MRBook/Assets/MRBook/Programmer/SceneManager/MotionTestSceneManager.cs
+++ MRBook/Assets/MRBook/Programmer/SceneManager/MotionTestSceneManager.cs
@@ -5,95 +5,95 @@ using UnityEngine.UI;
 
 public class MotionTestSceneManager : BaseManager<MotionTestSceneManager>
 {
-    //[SerializeField]
-    //ActorName actorName = ActorName.Urashima;
+    [SerializeField]
+    ActorName actorName = ActorName.Urashima;
 
-    //HoloCharacter character;
-    //[SerializeField]
-    //Element[] elements = null;
+    HoloCharacter character;
+    [SerializeField]
+    Element[] elements = null;
 
-    //[System.Serializable]
-    //class Element
-    //{
-    //    public MotionName name = MotionName.Wait;
-    //    public float transitionTime = 0.1f;
-    //}
+    [System.Serializable]
+    class Element
+    {
+        public MotionName name = MotionName.Wait;
+        public float transitionTime = 0.1f;
+    }
 
-    //[SerializeField]
-    //Text stateText = null;
+    [SerializeField]
+    Text stateText = null;
 
-    //int currentNameIndex = -1;
+    int currentNameIndex = -1;
 
-    //[SerializeField]
-    //HoloItem item = null;
+    [SerializeField]
+    HoloItem item = null;
 
-    //GameObject targetObj = null;
+    GameObject targetObj = null;
 
-    //Dictionary<string, HoloObject> actorDictionary = new Dictionary<string, HoloObject>();
+    Dictionary<string, HoloCharacter> actorDictionary = new Dictionary<string, HoloCharacter>();
 
-    //protected override void Start()
-    //{
-    //    base.Start();
+    protected override void Start()
+    {
+        base.Start();
 
-    //    targetObj = GameObject.FindGameObjectWithTag("Target");
+        targetObj = GameObject.FindGameObjectWithTag("Target");
 
-    //    GameObject[] tempArray = GameObject.FindGameObjectsWithTag("Actor");
+        GameObject[] tempArray = GameObject.FindGameObjectsWithTag("Actor");
 
-    //    for (int i = 0; i < tempArray.Length; i++)
-    //    {
-    //        HoloObject obj = tempArray[i].GetComponent<HoloObject>();
+        for (int i = 0; i < tempArray.Length; i++)
+        {
+            HoloObject obj = tempArray[i].GetComponent<HoloObject>();
 
-    //        if (obj.GetActorType == HoloObject.Type.Statics) return;
+            if (obj.GetActorType == HoloObject.Type.Character) return;
 
-    //        HoloMovableObject movableObject = (HoloMovableObject)obj;
-    //        movableObject.ApplyDefaultTransform();
-    //        actorDictionary.Add(movableObject.name, movableObject);
-    //    }
-    //}
+            HoloCharacter character = (HoloCharacter)obj;
+            character.PageStart(0);
+            actorDictionary.Add(character.name, character);
+        }
+    }
 
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.N))
-    //    {
-    //        currentNameIndex++;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            currentNameIndex++;
 
-    //        if (currentNameIndex >= elements.Length)
-    //        {
-    //            currentNameIndex = 0;
-    //        }
+            if (currentNameIndex >= elements.Length)
+            {
+                currentNameIndex = 0;
+            }
 
-    //        character = (HoloCharacter)(GetActor(actorName));
-    //        string motionName = MotionNameManager.GetMotionName(elements[currentNameIndex].name, character);
-    //        stateText.text = motionName;
-    //        Debug.Log("call change Animation " + motionName);
-    //        character.m_animator.CrossFade(motionName, elements[currentNameIndex].transitionTime);
-    //    }
-    //}
+            character =GetActor(actorName);
+            string motionName = MotionNameManager.GetMotionName(elements[currentNameIndex].name,(CharacterItemSaucer)character.ItemSaucer);
+            stateText.text = motionName;
+            Debug.Log("call change Animation " + motionName);
+            character.m_animator.CrossFade(motionName, elements[currentNameIndex].transitionTime);
+        }
+    }
 
-    //public void SetItem()
-    //{
-    //    character = (HoloCharacter)GetActor(actorName);
+    public void SetItem()
+    {
+        character = GetActor(actorName);
 
-    //    //character.SetItem(item.gameObject);
-    //}
+        character.ItemSaucer.SetItem(item);
+    }
 
-    //public void DumpItem()
-    //{
-    //    character = (HoloCharacter)GetActor(actorName);
-    //    //character.DumpItem(HoloItem.Hand.Both);
-    //}
+    public void DumpItem()
+    {
+        character = GetActor(actorName);
+        character.ItemSaucer.DumpItem();
+    }
 
-    //public void GoThere()
-    //{
-    //    Vector3 targetPosition = targetObj.transform.position;
-    //    targetPosition.y = 0.0f;
-    //    character = (HoloCharacter)GetActor(actorName);
-    //    character.m_agent.stoppingDistance = 0.01f;
-    //    character.m_agent.SetDestination(targetPosition);
-    //}
+    public void GoThere()
+    {
+        Vector3 targetPosition = targetObj.transform.position;
+        targetPosition.y = 0.0f;
+        character = GetActor(actorName);
+        character.m_agent.stoppingDistance = 0.01f;
+        character.m_agent.SetDestination(targetPosition);
+    }
 
-    //HoloObject GetActor(ActorName actorName)
-    //{
-    //    return actorDictionary[actorName.ToString()];
-    //}
+    HoloCharacter GetActor(ActorName actorName)
+    {
+        return actorDictionary[actorName.ToString()];
+    }
 }
