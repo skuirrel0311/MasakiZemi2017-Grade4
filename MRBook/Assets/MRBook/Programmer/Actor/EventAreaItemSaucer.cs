8b2fb3547feb08f861e655df1c0a8f178819e49a
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-12-20 19:11:08 +0900

    ちょっと拡張に難航中

--- MRBook/Assets/MRBook/Programmer/Actor/EventAreaItemSaucer.cs
+++ MRBook/Assets/MRBook/Programmer/Actor/EventAreaItemSaucer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 /// <summary>
@@ -8,5 +9,25 @@ using UnityEngine;
 /// </summary>
 public class EventAreaItemSaucer : BaseItemSaucer
 {
+    [SerializeField]
+    UnityEvent onSetItem = null;
 
+    [SerializeField]
+    string[] acceptObjectNames = null;
+
+    public override void SetItem(HoloItem item, bool showParticle = true)
+    {
+        if (onSetItem != null) onSetItem.Invoke();
+        if (showParticle) ParticleManager.I.Play("Doron", owner.transform.position, Quaternion.identity);
+    }
+
+    public override bool CheckCanHaveItem(HoloItem item)
+    {
+        for(int i = 0;i< acceptObjectNames.Length;i++)
+        {
+            if (item.name == acceptObjectNames[i]) return true;
+        }
+
+        return false;
+    }
 }
