6b02a5e5c4c3f723f6521826243faf7800cd5e1c
DESKTOP-92R0MHE\きくち <ritukizyuza@gmail.com>
2017-12-21 20:22:48 +0900

    【Design】水しぶきのエフェクト追加

--- MRBook/Assets/MRBook/Programmer/Test/TestObject.cs
+++ MRBook/Assets/MRBook/Programmer/Test/TestObject.cs
@@ -5,11 +5,14 @@ using UnityEngine.AI;
 
 public class TestObject : MonoBehaviour
 {
-    void Start()
+    public Vector3 pos;
+    public string particleName;
+
+    void Update()
     {
-        float x = 3.541592f;
-        float size = 8f;
-        Debug.Log("x = " + size * Mathf.Floor(x / size));
-        Debug.Log("x = " + Mathf.Floor(x));
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            ParticleManager.I.Play(particleName, pos);
+        }
     }
 }
