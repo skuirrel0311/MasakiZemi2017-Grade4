63b9bb254bd1fd8e5b461d752a8502b6a84dd1d5
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2017-12-27 15:21:54 +0900

    追従するお題を作り途中

---
+++ MRBook/Assets/MRBook/Programmer/UI/MissionTextController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissionTextController : MonoBehaviour
+{
+    public enum Mode { Track, Statics}
+
+    Mode currentMode;
+    Transform mainCameraTransform;
+    Transform MainCameraTransform
+    {
+        get
+        {
+            if (mainCameraTransform == null) mainCameraTransform = Camera.main.transform;
+            return mainCameraTransform;
+        }
+    }
+
+    [SerializeField]
+    Transform staticsPosition = null;
+
+    void Start()
+    {
+        MainSceneManager sceneManager = MainSceneManager.I;
+
+        //todo:ページの読み込みが終わった時にTrackになるように設定する
+    }
+
+    public void ChangeMode(Mode mode)
+    {
+        if (currentMode == mode) return;
+
+        currentMode = mode;
+
+        switch(mode)
+        {
+            case Mode.Statics:
+                transform.parent = MainGameUIController.I.transform;
+                transform.position = staticsPosition.position;
+                transform.rotation = staticsPosition.rotation;
+                break;
+            case Mode.Track:
+                transform.parent = MainCameraTransform;
+                transform.position = new Vector3(0.0f, 0.0f, 2.0f);
+                transform.rotation = Quaternion.identity;
+                break;
+        }
+    }
+}
