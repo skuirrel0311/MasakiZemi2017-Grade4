599df49c845f6850f9e3a0acabe454d20160ee7a
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2018-01-15 15:00:09 +0900

    ４ページ目マッチョ走り抜けていく

---
+++ MRBook/Assets/MRBook/Programmer/StateMachineBehavior/ChangeAnimatorController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimatorController : BaseStateMachineBehaviour
+{
+    [SerializeField]
+    ActorName characterName = ActorName.Urashima;
+    [SerializeField]
+    string animatorControllerName = "";
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        HoloCharacter character = ActorManager.I.GetCharacter(characterName);
+
+        if (character == null) return;
+
+        RuntimeAnimatorController animatorController = MyAssetStore.I.GetAsset<RuntimeAnimatorController>(animatorControllerName, "AnimatorControllers/");
+
+        if(animatorController == null)
+        {
+            Debug.Log("not found " + animatorControllerName);
+            return;
+        }
+
+        RuntimeAnimatorController oldAnimatorController = character.m_animator.runtimeAnimatorController;
+        Debug.Log("change animator controller");
+        character.m_animator.runtimeAnimatorController = animatorController;
+
+        MainSceneManager.I.OnReset += () => character.m_animator.runtimeAnimatorController = oldAnimatorController;
+    }
+}
