0e363d73325a20e8040da889c185868f1f728175
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2018-01-19 16:08:15 +0900

    貝殻の判定用スクリプト追加＋貝殻のマーカーを更新

---
+++ MRBook/Assets/MRBook/Programmer/Vuforia/ShellMaker.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellMaker : MonoBehaviour
+{
+    [SerializeField]
+    ShellTracableEventHandler shellFront = null;
+    [SerializeField]
+    ShellTracableEventHandler shellBack = null;
+    [SerializeField]
+    GameObject treasureObject = null;
+
+    Transform cameraTransform;
+
+    bool isCloseShell = true;
+    bool IsCloseShell
+    {
+        get
+        {
+            return isCloseShell;
+        }
+        set
+        {
+            if (isCloseShell == value) return;
+            if (!IsNearCamera()) return;
+
+            isCloseShell = value;
+            ChangeShellState(isCloseShell);
+        }
+    }
+
+    bool IsNearCamera()
+    {
+        float distance = Vector3.Distance(cameraTransform.position, treasureObject.transform.position);
+        Debug.Log("distance = " + distance);
+        return true;
+    }
+
+    void Start()
+    {
+        cameraTransform = Camera.main.transform;
+
+#if !UNITY_EDITOR
+
+        shellFront.onFound += () =>
+        {
+            IsCloseShell = true;
+        };
+
+        shellBack.onFound += () =>
+        {
+            IsCloseShell = false;
+        };
+#endif
+    }
+
+#if UNITY_EDITOR
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            NotificationManager.I.ShowMessage(!IsCloseShell ? "貝殻を開けました" : "貝殻を閉じました");
+            IsCloseShell = !IsCloseShell;
+        }
+    }
+#endif
+
+    void ChangeShellState(bool isClose)
+    {
+        treasureObject.gameObject.SetActive(!isClose);
+    }
+}
---
+++ MRBook/Assets/MRBook/Programmer/Vuforia/ShellTracableEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellTracableEventHandler : MyTracableEventHandler
+{
+    public Action onFound;
+
+    protected override void OnTrackingFound()
+    {
+        if (onFound != null) onFound.Invoke();
+    }
+
+    protected override void OnTrackingLost() { }
+}
