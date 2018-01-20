516f97621fd4f18c6e5178998ed8994b239e2b3e
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2018-01-20 14:53:52 +0900

    文字を表示する時間を指定できるようにした。

--- MRBook/Assets/MRBook/Programmer/StateMachineBehavior/TestBehavior.cs
+++ MRBook/Assets/MRBook/Programmer/StateMachineBehavior/TestBehavior.cs
@@ -5,10 +5,13 @@ public class TestBehavior : BaseStateMachineBehaviour
     [Multiline]
     public string message;
 
+    [SerializeField]
+    float lifeTime = 5.0f;
+
     protected override void OnStart()
     {
         base.OnStart();
         MissionTextController.I.AddText(message);
-        NotificationManager.I.ShowMessage(message, 5.0f);
+        NotificationManager.I.ShowMessage(message, lifeTime);
     }
 }
