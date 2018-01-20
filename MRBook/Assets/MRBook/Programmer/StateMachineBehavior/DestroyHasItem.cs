a7d47589dad486b367e35d969fd7e134239df3af
DESKTOP-O06CIUB\Kazuki <k.kaneko0311@gmail.com>
2018-01-20 20:08:04 +0900

    アイテムを捨てるStateMachineBehaviourを追加

---
+++ MRBook/Assets/MRBook/Programmer/StateMachineBehavior/DestroyHasItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// dumpItemしてからdestroyObjectするやつ
+/// </summary>
+public class DestroyHasItem : BaseStateMachineBehaviour
+{
+    [SerializeField]
+    ActorName actorName = ActorName.Urashima;
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+
+        HoloCharacter character = ActorManager.I.GetCharacter(actorName);
+
+        if (character == null)
+        {
+            return;
+        }
+        CharacterItemSaucer characterItemSaucer = (CharacterItemSaucer)character.ItemSaucer;
+
+        characterItemSaucer.DumpItem();
+
+    }
+}
