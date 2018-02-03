bfa687c141fb902aad3b17ba4046cf1060bd14cd
skuirrel0311 <rureikus7223@gmail.com>
2018-02-03 21:27:13 +0900

    ５ページ目も一応対応

--- MRBook/Assets/MRBook/Programmer/GameManager/PageFiveManager.cs
+++ MRBook/Assets/MRBook/Programmer/GameManager/PageFiveManager.cs
@@ -5,22 +5,11 @@ using KKUtilities;
 
 public class PageFiveManager : BasePage
 {
-    [SerializeField]
-    HoloCharacter urashima = null;
-    [SerializeField]
-    HoloCharacter turtle = null;
-    [SerializeField]
-    HoloItem secretBox_Box = null;
-    [SerializeField]
-    HoloItem secretBox_Lid = null;
-
     public override void PageStart()
     {
         base.PageStart();
 
         MainSceneManager sceneManager = MainSceneManager.I;
-
-        sceneManager.OnPageLoaded += AddPageCharacter;
 
         sceneManager.OnPlayEnd += (success) =>
         {
@@ -32,15 +21,5 @@ public class PageFiveManager : BasePage
             }, this);
 
         };
-    }
-
-    void AddPageCharacter(BasePage page)
-    {
-        ActorManager.I.AddCharacter(ActorName.Urashima, urashima);
-        ActorManager.I.AddCharacter(ActorName.Turtle, turtle);
-        ActorManager.I.AddObject(secretBox_Box);
-        ActorManager.I.AddObject(secretBox_Lid);
-
-        MainSceneManager.I.OnPageLoaded -= AddPageCharacter;
     }
 }
